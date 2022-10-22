06b4264d9d1d164434e94eaac4fc473e142a8a88
Ruslan <rusl.babae@yandex.ru>
2022-10-22 10:58:28 +0300

    1

--- 3/task_2.py
+++ 3/task_2.py
@@ -1,16 +1,19 @@
-print('Доброго времени суток!\nДанная программа может определить, является ли число, '
-      'введенное пользователем, чётным или нет.')
+print('Доброго времени суток!\nДанная программа выводит все простые числа вплоть до заданного.')
 
-n = input("n=")
-a = range(n+1)
-a[1] = 0
-lst = []
+try:
+    n = int(input('Введите число: '))
+    if n < 2:
+        raise ValueError
+    a = list(range(n + 1))
+    res = []
 
-i = 2
-while i <= n:
-    if a[i] != 0:
-        lst.append(a[i])
-        for j in xrange(i, n+1, i):
-            a[j] = 0
-    i += 1
-print lst
+    i = 2
+    while i <= n:
+        if a[i]:
+            res.append(a[i])
+            for j in range(i, n + 1, i):
+                a[j] = 0
+        i += 1
+    print(f'Cписок простых чисел в диапазоне от 2 до {n}:\n{", ".join([str(i) for i in res])}')
+except Exception:
+    print('Некорректный ввод!')
--- 3/task_3.py
+++ 3/task_3.py
@@ -1,5 +1,9 @@
-n = float(input())
-if n % 2:
-    print(f'Число {n} - нечетное')
-else:
-    print(f'Число {n} - четное')
+print('Доброго времени суток!\nДанная программа определяет наибольшее из трёх чисел, введенных пользователем.')
+
+a, b, c = tuple([float(i) for i in input('Введите три числа через пробел: ').split()])
+maximum = c
+if a > b >= c:
+    maximum = a
+elif b > c:
+    maximum = b
+print(f'Наибольшее число: {maximum}')
